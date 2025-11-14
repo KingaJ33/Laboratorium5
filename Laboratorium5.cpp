b27147b21fc0512b90f423ac126834285651236c
Kinga <kinga.janiak@student.put.poznan.pl>
2025-11-14 14:39:34 +0100

    nowe

--- Laboratorium5.cpp
+++ Laboratorium5.cpp
@@ -2,11 +2,111 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+//ZAD.1
+
+int main() {
+
+    int tab[10];
+    int n;
+
+    cout << "wprowadz 10 liczb calkowitych do tablicy: " << endl;
+
+    for (int i = 0; i < 10; i++) {
+        cin >> n;
+        tab[i] = n;
+
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << tab[i] << "  ";
+
+    }
+
+    return 0;
 }
+
+
+
+
+
+
+
+//ZAD.2
+/*
+
+void ciag(int n);
+
+int main() {
+    int n;
+    cout << "Podaj liczbe calkowita dodatnia: ";
+    cin >> n;
+
+   
+    if (n > 0) {
+        ciag(n);
+    }
+    else {
+        cout << "Liczba musi byc calkowita dodatnia" << endl;
+    }
+
+    return 0;
+}
+
+
+void ciag(int n) {
+    cout << "dzielniki liczby: " << endl;
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << endl;
+        }
+    }
+}
+
+    //ZAD.3
+    /*
+
+        int tab[10];
+        int suma = 0;
+
+        srand(time(0)); 
+
+        
+        for (int i = 0; i < 10; i++) {
+            tab[i] = rand() % 100;
+            cout << tab[i] << " ";
+            suma += tab[i];
+        }
+
+        double srednia = suma / 10.0;
+        cout << "\nŚrednia: " << srednia << endl;
+
+        
+        int mniejsze = 0;
+        for (int i = 0; i < 10; i++) {
+            if (tab[i] < srednia) mniejsze++;
+        }
+        cout << "Liczb mniejszych od średniej: " << mniejsze << endl;
+
+     
+        int min = tab[0], max = tab[0];
+        for (int i = 1; i < 10; i++) {
+            if (tab[i] < min) min = tab[i];
+            if (tab[i] > max) max = tab[i];
+        }
+        cout << "Najmniejsza: " << min << ", Największa: " << max << endl;
+
+        */
+
+    //ZAD.4
+
+
+
+
+
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
 // Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
